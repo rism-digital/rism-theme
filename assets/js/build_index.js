9e6c3c6fb5ca75f2bb28eff82ea1e40ba6feb92c
Laurent Pugin <lxpugin@gmail.com>
2021-09-16 13:37:46 +0200

    Version 0.1.60
    * Add support for multi-lang
    * Adjust header to use the _data/local.yml translations
    * Add option not to show the search box

--- assets/js/build_index.js
+++ assets/js/build_index.js
@@ -3,6 +3,12 @@ var lunr = require('lunr'),
     stdout = process.stdout,
     buffer = []
 
+require( "lunr-languages/lunr.stemmer.support" )( lunr )
+require( "lunr-languages/lunr.multi" )( lunr )
+require( "lunr-languages/lunr.de" )( lunr )
+require( "lunr-languages/lunr.fr" )( lunr )
+require( "lunr-languages/lunr.it" )( lunr )
+
 stdin.resume()
 stdin.setEncoding('utf8')
 
@@ -13,10 +19,13 @@ stdin.on('data', function (data) {
 stdin.on('end', function () {
   var documents = JSON.parse(buffer.join())
 
-  var idx = lunr(function () {
+  var idx = lunr( function ()
+  {
+    this.use(lunr.multiLanguage('en', 'de', 'fr', 'it'))
     this.ref('id')
     this.field( 'title' )
-    this.field('body')
+    this.field( 'body' )
+    this.field( 'lang' )
 
     documents.forEach(function (doc) {
       this.add(doc)
