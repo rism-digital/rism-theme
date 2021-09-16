524cd91e954c985c86b39cec9c744f1331c5a4cd
Laurent Pugin <lxpugin@gmail.com>
2021-09-16 17:14:22 +0200

    Change script not to use stdin / stdout

--- assets/js/build_index.js
+++ assets/js/build_index.js
@@ -1,7 +1,5 @@
-var lunr = require('lunr'),
-    stdin = process.stdin,
-    stdout = process.stdout,
-    buffer = []
+const fs = require( 'fs' );
+const lunr = require( 'lunr' );
 
 require( "lunr-languages/lunr.stemmer.support" )( lunr )
 require( "lunr-languages/lunr.multi" )( lunr )
@@ -9,28 +7,43 @@ require( "lunr-languages/lunr.de" )( lunr )
 require( "lunr-languages/lunr.fr" )( lunr )
 require( "lunr-languages/lunr.it" )( lunr )
 
-stdin.resume()
-stdin.setEncoding('utf8')
+// Expects two arguments: input and output files
+var scriptArgs = process.argv.slice( 2 );
+var inputFile = scriptArgs[0];
+var outputFile = scriptArgs[1];
 
-stdin.on('data', function (data) {
-  buffer.push(data)
-})
+var documents;
+// Read the input file
+fs.readFile( inputFile, 'utf8', ( err, data ) =>
+{
+    if (err) {
+        console.error(err)
+        return
+    }
+    documents = JSON.parse( data );
+    
+    // Build the index
+    var idx = lunr( function ()
+    {
+        this.use( lunr.multiLanguage( 'en', 'de', 'fr', 'it' ) )
+        this.ref( 'id' )
+        this.field( 'title' )
+        this.field( 'body' )
+        this.field( 'lang' )
 
-stdin.on('end', function () {
-  var documents = JSON.parse(buffer.join())
+        documents.forEach( function ( doc )
+        {
+            this.add( doc )
+        }, this )
+    } );
 
-  var idx = lunr( function ()
-  {
-    this.use(lunr.multiLanguage('en', 'de', 'fr', 'it'))
-    this.ref('id')
-    this.field( 'title' )
-    this.field( 'body' )
-    this.field( 'lang' )
-
-    documents.forEach(function (doc) {
-      this.add(doc)
-    }, this)
-  })
-
-  stdout.write(JSON.stringify(idx))
+    // Write the index file
+    fs.writeFile( outputFile, JSON.stringify( idx ), err =>
+    {
+        if ( err )
+        {
+            console.error( err )
+            return;
+        }
+    })
 })
